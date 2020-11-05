34767059efa90abbaf8cd04d68a3ea79be10857e
Jon Chen <bsd@voltaire.sh>
2020-11-05 20:08:42 +0000

    sign message if key is available

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bsdlp/notify-docker-pull
 
 go 1.15
 
-require github.com/bsdlp/update-docker-image v0.0.0-20201105054545-47e8da7c4229
+require github.com/bsdlp/update-docker-image v0.0.0-20201105200224-b35146985022
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/ed25519"
+	"encoding/base64"
 	"flag"
 	"log"
 	"net/http"
@@ -19,14 +21,29 @@ func main() {
 	if image == "" {
 		log.Fatal("image is required")
 	}
+	encodedPrivateKey := flag.Arg(2)
+	var privateKey ed25519.PrivateKey
+	if encodedPrivateKey != "" {
+		decodedKey, err := base64.StdEncoding.DecodeString(encodedPrivateKey)
+		if err != nil {
+			log.Fatal("invalid private key")
+		}
+		privateKey = ed25519.PrivateKey(decodedKey)
+	}
 
 	log.Printf("calling server '%s'", baseURL)
 	log.Printf("sending notification for image '%s'", image)
 
-	client := update_docker_image.NewUpdateDockerImageProtobufClient(baseURL, &http.Client{})
-	_, err := client.UpdateImage(context.Background(), &update_docker_image.UpdateImageReq{
+	req := &update_docker_image.UpdateImageReq{
 		Image: image,
-	})
+	}
+
+	if privateKey != nil {
+		req.Signature = ed25519.Sign(privateKey, []byte(image))
+	}
+
+	client := update_docker_image.NewUpdateDockerImageProtobufClient(baseURL, &http.Client{})
+	_, err := client.UpdateImage(context.Background(), req)
 	if err != nil {
 		log.Fatal("error calling UpdateImage: " + err.Error())
 	}
